c42fa253acb0cd83c1727eae4546adeedfa2c5e3
Jason Lin <jkhlin@uci.edu>
2025-07-09 02:29:43 -0700

    create page is working and also working on product.js for global state management

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,6 +1,56 @@
+import { useState } from "react";
+import { Container, Heading, VStack, Box, useColorModeValue, Input, Button } from "@chakra-ui/react";
+
 const CreatePage = () => {
+    const [newProduct, setNewProduct] = useState({
+        name: "",
+        price: "",
+        image: ""
+    })
+
+    const handleAddProduct = () => {
+        console.log("Product added:", newProduct);
+    }
+
     return (
-        <div>CreatePage</div>
+        <Container maxW={"container.sm"}>
+            <VStack>
+                <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+                    Create New Product
+                </Heading>
+
+                <Box w={"full"} bg={useColorModeValue("white", "gray.700")}
+                    p={6} rounded={"large"} shadow={"md"}>
+                    <VStack spacing={4}>
+                        <Input 
+                        placeholder="Product Name"
+                        name="name"
+                        value={newProduct.name}
+                        onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+                        />
+
+                        <Input 
+                        placeholder="Price"
+                        name="price"
+                        value={newProduct.price}
+                        onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
+                        />
+
+                        <Input
+                        placeholder="Image URL"
+                        name="image"
+                        value={newProduct.image}
+                        onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })}
+                        />
+
+                        <Button colorScheme="blue" w={"full"} onClick={handleAddProduct}>
+                            Add Product
+                        </Button>
+
+                    </VStack>
+                </Box>
+            </VStack>
+        </Container>
     )
 }
 
