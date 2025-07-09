b6b3edef9616d472b3634a36723b393e346c26e1
Jason Lin <jkhlin@uci.edu>
2025-07-08 23:58:49 -0700

    finished navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
-import { Button, Box } from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react"
 import { Routes, Route } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import CreatePage from "./pages/CreatePage"
+import Navbar from "./components/Navbar"
+import { useColorModeValue } from "@chakra-ui/react"
 
 function App() {
 
   return (
-    <Box minH={"100vh"}>
-      {/* <Navbar /> */}
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,50 @@
+import { Button, Container, Flex, HStack, Text } from "@chakra-ui/react"
+import { Link } from "react-router-dom";
+import { CiSquarePlus } from "react-icons/ci";
+import { useColorMode } from "@chakra-ui/react";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
+
 const Navbar = () => {
+    const { colorMode, toggleColorMode } = useColorMode();
+
     return (
-        <div>Navbar</div>
+        <Container maxW={"1140px"} px={4}>
+            <Flex 
+                h={16} 
+                alignItems={"center"} 
+                justifyContent={"space-between"}
+                flexDir={{
+                    base: "column",
+                    sm: "row",
+                }}
+            >
+
+                <Text
+                    fontSize={{base: "22", sm: "24"}}
+                    fontWeight="bold"
+                    textTransform={"uppercase"}
+                    textAlign={"center"}
+                    bgGradient={"linear(to-r, teal.500, blue.500)"}
+                    bgClip={"text"}
+                >
+                    <Link to={"/"}>Product Store</Link>
+                </Text>
+
+                <HStack spacing={2} alignItems={"center"}>
+                    <Link to={"/create"}> 
+                        <Button>
+                            <CiSquarePlus fontSize={20}/>
+                        </Button>
+                    </Link>
+
+                    <Button onClick={toggleColorMode}>
+                        {colorMode === "light" ? <IoMoon fontSize={20} /> : <LuSun fontSize={20} />}
+                    </Button>
+                </HStack>
+
+            </Flex>
+        </Container>
     )
 }
 
