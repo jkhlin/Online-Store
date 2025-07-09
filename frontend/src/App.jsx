92dc2257290af6d8929cba8f62fc1a0b0f5d5062
Jason Lin <jkhlin@uci.edu>
2025-07-08 20:22:52 -0700

    base components and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
-import { Button } from "@chakra-ui/react"
-
+import { Button, Box } from "@chakra-ui/react"
+import { Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
-    <>
-    <Button>Hello</Button>
-    </>
+    <Box minH={"100vh"}>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
   )
 }
 
