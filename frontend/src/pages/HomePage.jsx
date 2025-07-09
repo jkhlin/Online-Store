0a2922e0181796ebe2cafe0b38b5fb480237837d
Jason Lin <jkhlin@uci.edu>
2025-07-09 16:12:18 -0700

    homepage skeleton layout done

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,19 @@
+import { Container, VStack, Text } from "@chakra-ui/react";
+
 const HomePage = () => {
     return (
-        <div>HomePage</div>
+        <Container maxW={"container.xl"} py={12} bg={"green.500"}>
+            <VStack>
+                <Text
+                    fontSize={30}
+                    fontWeight={"bold"}
+                    bgGradient={"linear(to-r, cyan.200, blue.500)"}
+                    bgClip={"text"}
+                >
+                    Current Products! 
+                </Text>
+            </VStack>
+        </Container>
     )
 }
 
