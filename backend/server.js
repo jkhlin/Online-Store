d2ff61b459de7f5505cbef2061e3d899718a3753
Jason Lin <jkhlin@uci.edu>
2025-07-15 02:34:58 -0700

    working on getting client and server hosted on same port

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./config/db.js";
+import { connectDB } from "./config/db.js";
+import path from "path";
 
 import router from "./routes/route.js";
 
@@ -9,9 +10,15 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
+const __dirname = path.resolve(); // current working directory
+
 app.use(express.json()); // Middleware to parse JSON bodies
 
 app.use("/api/products", router); // Serves as an prefix alias for the product routes
+
+if (process.env.NODE_ENV === "production") {
+
+}
 
 app.listen(port, () => {
     connectDB()
